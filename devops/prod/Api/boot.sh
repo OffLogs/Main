74aca87a0917ce7bb4470cc22e054d3b8c4290b7
lampego <lampego@gmail.com>
2021-03-08 12:39:03 +0200

    Removed migrations Dockerfile

--- devops/prod/Api/boot.sh
+++ devops/prod/Api/boot.sh
@@ -5,5 +5,10 @@
 #    sleep 1
 #done
 
+>&2 echo "Run Migrations"
+cd ../OffLogs.Migrations
+dotnet run --configuration Development
+
 >&2 echo "SQL Server is up - executing command"
+cd ../OffLogs.Api
 dotnet run --no-launch-profile
