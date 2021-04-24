d6bf218b7284fce27086703aa38d0aec112e028a
lampego <lampego@gmail.com>
2021-04-24 07:23:43 +0300

    #227 Added new pages

---
+++ OffLogs.Web/Core/Constants/SiteUrl.cs
@@ -0,0 +1,10 @@
+namespace OffLogs.Web.Core.Constants
+{
+    public class SiteUrl
+    {
+        public static readonly string Main = "/";
+        
+        public static readonly string Dashboard = "/dashboard";
+        public static readonly string DashboardApplications = "/dashboard/applications";
+    }
+}
