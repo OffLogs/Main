dacd0c0ba9719bb6a86f82359ddeda4daf2cb173
lampego <lampego@gmail.com>
2021-04-26 16:38:20 +0300

    #227 Add error class

--- OffLogs.Web/Core/Components/Form/CustomInputText.cs
+++ OffLogs.Web/Core/Components/Form/CustomInputText.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Diagnostics.CodeAnalysis;
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Forms;
@@ -7,7 +8,14 @@ namespace OffLogs.Web.Core.Components.Form
 {
     public class CustomInputText : InputBase<string?>
     {
-        [Parameter] public string? Label { get; set; }
+        [Parameter] 
+        public string? Label { get; set; }
+        
+        [Parameter] 
+        public string? Description { get; set; }
+        
+        [Parameter] 
+        public string? ParentClass { get; set; }
 
         /// <inheritdoc />
         protected override void BuildRenderTree(RenderTreeBuilder builder)
@@ -15,22 +23,32 @@ namespace OffLogs.Web.Core.Components.Form
             var sequence = 0;
             
             builder.OpenElement(sequence++, "div");
-            builder.AddAttribute(sequence++, "class", "input-group mb-3");
+            if (!string.IsNullOrEmpty(ParentClass))
             {
-                builder.OpenElement(sequence++, "div");
-                builder.AddAttribute(sequence++, "class", "input-group-prepend");
+                builder.AddAttribute(sequence++, "class", ParentClass);
+            }
+            {
+                if (!string.IsNullOrEmpty(Label))
                 {
                     // Label
-                    builder.OpenElement(sequence++, "span");
-                    builder.AddAttribute(sequence++, "class", "input-group-text");
+                    builder.OpenElement(sequence++, "label");
+                    builder.AddAttribute(sequence++, "class", "input-group-prepend");
                     builder.AddContent(sequence++, Label);
                     builder.CloseElement();
                 }
-                builder.CloseElement();
-            
+                if (!string.IsNullOrEmpty(Description))
+                {
+                    builder.OpenElement(sequence++, "div");
+                    builder.AddAttribute(sequence++, "class", "form-text");
+                    builder.AddContent(sequence++, Description);
+                    builder.CloseElement();
+                }
+
+                var bootstrapCssFiles = CssClass.Replace("invalid", "is-invalid");
+                
                 builder.OpenElement(sequence++, "input");
                 builder.AddMultipleAttributes(sequence++, AdditionalAttributes);
-                builder.AddAttribute(sequence++, "class", CssClass);
+                builder.AddAttribute(sequence++, "class", $"{bootstrapCssFiles} form-control");
                 builder.AddAttribute(sequence++, "value", BindConverter.FormatValue(CurrentValue));
                 builder.AddAttribute(sequence++, "onchange", EventCallback.Factory.CreateBinder<string?>(this, __value => CurrentValueAsString = __value, CurrentValueAsString));
                 builder.CloseElement();
