e80ea791176acd3bdea498ff0432ca9d54fea723
lampego <lampego@gmail.com>
2021-07-02 22:04:50 +0300

    #206 list loading

--- OffLogs.Web/Shared/Ui/Form/CustomDropDown/DropDownListItem.cs
+++ OffLogs.Web/Shared/Ui/Form/CustomDropDown/DropDownListItem.cs
@@ -6,5 +6,13 @@ namespace OffLogs.Web.Shared.Ui.Form.CustomDropDown
         public string Label { get; set; }
         public string Description { get; set; }
         public bool IsSelected { get; set; }
+
+        public int IdAsInt
+        {
+            get {
+                int.TryParse(Id, out var parsedValue);
+                return parsedValue;
+            }
+        }
     }
 }
