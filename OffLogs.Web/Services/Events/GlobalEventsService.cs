899e87f4aa910671c8efa103f295bac568138c8a
lampego <lampego@gmail.com>
2021-07-02 06:49:51 +0300

    Partial commit

---
+++ OffLogs.Web/Services/Events/GlobalEventsService.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OffLogs.Web.Services.Events
+{
+    public class GlobalEventsService: IGlobalEventsService
+    {
+        public event Action OnClickDocument;
+        public Task InvokeOnClickDocumentAsync()
+        {
+            return Task.Run(() =>
+            {
+                OnClickDocument?.Invoke();
+            });
+        }
+    }
+}
