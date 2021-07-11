6b9a731ce7679aefe179a05dbfe876a1e993b3ed
lampego <lampego@gmail.com>
2021-07-11 16:23:17 +0300

    #239 Finished logs list. Added LogLevel filter

---
+++ OffLogs.Migrations/Migrations/10_CreateIndexes.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using OffLogs.Migrations.Code;
+
+namespace OffLogs.Migrations.Migrations
+{
+    [Migration(10)]
+    public class _10_CreateIndexes : MyMigration
+    {
+        public override void Up()
+        {
+            // logs
+            Create.Index("ix_logs_level")
+                .OnTable("logs")
+                .OnColumn("level");
+
+            base.Up();
+        }
+    }
+}
---
+++ OffLogs.Migrations/Migrations/11_CreateIndexes.cs
@@ -0,0 +1,19 @@
+﻿using FluentMigrator;
+using OffLogs.Migrations.Code;
+
+namespace OffLogs.Migrations.Migrations
+{
+    [Migration(11)]
+    public class _11_CreateIndexes : MyMigration
+    {
+        public override void Up()
+        {
+            // logs
+            Create.Index("ix_logs_log_time")
+                .OnTable("logs")
+                .OnColumn("log_time");
+            
+            base.Up();
+        }
+    }
+}
