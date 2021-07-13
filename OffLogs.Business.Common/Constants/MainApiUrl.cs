ad5fed7887ea3bc7d9f06c66ab3cefd2e2a5aa48
lampego <lampego@gmail.com>
2021-07-13 07:59:04 +0300

    #3 Test issue

--- OffLogs.Business.Common/Constants/MainApiUrl.cs
+++ OffLogs.Business.Common/Constants/MainApiUrl.cs
@@ -12,5 +12,6 @@ namespace OffLogs.Business.Common.Constants
         public const string LogList = "/board/log/list";
         public const string LogGet = "/board/log/get";
         public const string LogSetIsFavorite = "/board/log/setFavorite";
+        
     }
 }
