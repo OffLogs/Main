999c0af3067f24fc1b2e7a5630f8163ff27d8cd2
lampego <lampego@gmail.com>
2021-07-22 22:28:52 +0300

    #9 Added API request: AddAccess

---
+++ OffLogs.Api/Controller/Board/Permission/Actions/AddAccessRequest.cs
@@ -0,0 +1,21 @@
+﻿using Api.Requests.Abstractions;
+using OffLogs.Business.Common.Constants.Permissions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OffLogs.Api.Controller.Board.Permission.Actions
+{
+    public class AddAccessRequest: IRequest
+    {
+        [Required]
+        [EnumDataType(typeof(PermissionAccessType))]
+        public PermissionAccessType AccessType { get; set; }
+
+        public int RecepientId { get; set; }
+
+        public int ItemId { get; set; }
+    }
+}
---
+++ OffLogs.Api/Controller/Board/Permission/Actions/AddAccessRequestHandler.cs
@@ -0,0 +1,48 @@
+﻿using Api.Requests.Abstractions;
+using OffLogs.Business.Common.Constants.Permissions;
+using OffLogs.Business.Orm.Entities;
+using OffLogs.Business.Services.Api;
+using OffLogs.Business.Services.Entities.Application;
+using Queries.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OffLogs.Api.Controller.Board.Permission.Actions
+{
+    public class AddAccessRequestHandler : IAsyncRequestHandler<AddAccessRequest>
+    {
+        private readonly IAsyncQueryBuilder _queryBuilder;
+        private readonly IRequestService _requestService;
+        private readonly IApplicationService _applicationService;
+
+        public AddAccessRequestHandler(
+            IAsyncQueryBuilder queryBuilder,
+            IRequestService requestService,
+            IApplicationService applicationService
+        )
+        {
+            _queryBuilder = queryBuilder ?? throw new ArgumentNullException(nameof(queryBuilder));
+            _requestService = requestService ?? throw new ArgumentNullException(nameof(requestService));
+            _applicationService = applicationService ?? throw new ArgumentNullException(nameof(applicationService));
+        }
+
+        public async Task ExecuteAsync(AddAccessRequest request)
+        {
+            var userId = _requestService.GetUserIdFromJwt();
+            if (request.AccessType == PermissionAccessType.ApplicationRead)
+            {
+                await AddApplicationReadRights(request);
+            }
+            var list = await _queryBuilder.For<ICollection<UserEntity>>()
+                .WithAsync(new UserSearchCriteria(request.Search, new long[] { userId }));
+        }
+
+        private Task AddApplicationReadRights(AddAccessRequest request)
+        {
+            var recepient = await _applicationService.ShareForUser();
+        }
+    }
+}
