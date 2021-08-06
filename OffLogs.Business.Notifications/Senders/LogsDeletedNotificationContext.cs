d6960457925511028b4b62f436aa70fd64b33af3
Heorhii Shyra <hshy@isd.dp.ua>
2021-08-06 11:34:02 +0300

    #49 First commit

---
+++ OffLogs.Business.Notifications/Senders/LogsDeletedNotificationContext.cs
@@ -0,0 +1,21 @@
+﻿using Notification.Abstractions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OffLogs.Business.Notifications.Senders
+{
+    public class LogsDeletedNotificationContext : INotificationContext
+    {
+        public LogsDeletedNotificationContext(string to, int deletedLogsCount)
+        {
+            To = to;
+            DeletedLogsCount = deletedLogsCount;
+        }
+
+        public string To { get; }
+        public int DeletedLogsCount { get; }
+    }
+}
---
+++ OffLogs.Business.Notifications/Senders/LogsDeletedNotificationSender.cs
@@ -0,0 +1,33 @@
+﻿using Notification.Abstractions;
+using OffLogs.Business.Notifications.Core.Emails;
+using OffLogs.Business.Notifications.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace OffLogs.Business.Notifications.Senders
+{
+    public class LogsDeletedNotificationSender : IAsyncNotification<LogsDeletedNotificationContext>
+    {
+        private readonly IEmailSendingService _emailSendingService;
+        private readonly EmailFactory _emailFactory;
+
+        public LogsDeletedNotificationSender(IEmailSendingService emailSendingService)
+        {
+            _emailSendingService = emailSendingService;
+            _emailFactory = new EmailFactory();
+        }
+
+        public Task SendAsync(LogsDeletedNotificationContext commandContext, CancellationToken cancellationToken = default)
+        {
+            var emailBuilder = _emailFactory.GetEmailBuilder("LogsDeletedNotification.htm");
+            emailBuilder.AddPlaceholder("deletedLogsCount", commandContext.DeletedLogsCount.ToString());
+            _emailSendingService.SendEmail(commandContext.To, emailBuilder, null);
+            // Some sending logic
+            return Task.CompletedTask;
+        }
+    }
+}
