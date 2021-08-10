2987106589f9acf1fcc99cb5c43b56bc5101c2ea
Heorhii Shyra <hshy@isd.dp.ua>
2021-08-10 12:11:40 +0300

    #68 Added DB command

---
+++ OffLogs.Api.Tests.Integration/Api/Main/Db/Application/DeleteApplicationTests.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using OffLogs.Api.Tests.Integration.Core;
+using OffLogs.Business.Common.Constants;
+using OffLogs.Business.Constants;
+using OffLogs.Business.Orm.Commands.Context;
+using OffLogs.Business.Orm.Commands.Entities.Application;
+using OffLogs.Business.Orm.Commands.Entities.Log;
+using OffLogs.Business.Orm.Entities;
+using OffLogs.Business.Orm.Queries;
+using Xunit;
+
+namespace OffLogs.Api.Tests.Integration.Api.Main.Db.Application
+{
+    public class DeleteApplicationTests : MyApiIntegrationTest
+    {
+        public DeleteApplicationTests(ApiCustomWebApplicationFactory factory) : base(factory) { }
+
+        [Fact]
+        public async Task ShouldDeleteOne()
+        {
+            var userModel = await DataSeeder.CreateNewUser();
+            var application = userModel.Applications.First();
+
+            var log1 = await CreateLog(application, LogLevel.Error);
+            await DbSessionProvider.PerformCommitAsync();
+
+            await CommandBuilder.ExecuteAsync(new ApplicationDeleteCommandContext(application.Id));
+            await DbSessionProvider.PerformCommitAsync();
+
+            // Clear session
+            DbSessionProvider.CurrentSession.Clear();
+
+            var actualApplication = await QueryBuilder.FindByIdAsync<ApplicationEntity>(application.Id);
+            Assert.Null(actualApplication);
+            var actualLog1 = await QueryBuilder.FindByIdAsync<LogEntity>(log1.Id);
+            Assert.Null(actualLog1);
+        }
+
+        private async Task<LogEntity> CreateLog(ApplicationEntity application, LogLevel level)
+        {
+            return await LogService.AddAsync(
+                application,
+                "SomeMessage",
+                level,
+                DateTime.UtcNow
+            );
+        }
+    }
+}
---
+++ OffLogs.Business.Orm/Commands/Entities/Application/ApplicationDeleteCommandContext.cs
@@ -0,0 +1,14 @@
+﻿using Commands.Abstractions;
+
+namespace OffLogs.Business.Orm.Commands.Entities.Application
+{
+    public class ApplicationDeleteCommandContext : ICommandContext
+    {
+        public ApplicationDeleteCommandContext(long applicationId)
+        {
+            ApplicationId = applicationId;
+        }
+
+        public long ApplicationId { get; }
+    }
+}
