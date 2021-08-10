e9c9f201ff6c09feaf21c42d5f869d44acd0fd7e
Heorhii Shyra <hshy@isd.dp.ua>
2021-08-10 12:01:36 +0300

    #68 Added migration

---
+++ OffLogs.Migrations/Migrations/19_AlterForeignKey_ApplicationLogs.cs
@@ -0,0 +1,20 @@
+﻿using FluentMigrator;
+using OffLogs.Migrations.Code;
+
+namespace OffLogs.Migrations.Migrations
+{
+    [Migration(19)]
+    public class _19_AlterForeignKey_ApplicationLogs : MyMigration
+    {
+        public override void Up()
+        {
+            Delete.ForeignKey("FK_logs_application_id_applications_id").OnTable("logs");
+
+            Create.ForeignKey()
+                .FromTable("logs").ForeignColumn("application_id")
+                .ToTable("applications").PrimaryColumn("id")
+                .OnDelete(System.Data.Rule.Cascade);
+            base.Up();
+        }
+    }
+}
