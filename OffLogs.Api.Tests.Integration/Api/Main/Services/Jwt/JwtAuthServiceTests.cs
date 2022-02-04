f3ccb2071542de8d938870357e49818dab15f6d3
lampego <lampego@gmail.com>
2022-02-04 23:33:28 +0200

    Fix tests. Upgrade libs

---
+++ OffLogs.Api.Tests.Integration/Api/Main/Services/Jwt/JwtAuthServiceTests.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Threading.Tasks;
+using OffLogs.Business.Orm.Entities;
+using Xunit;
+
+namespace OffLogs.Api.Tests.Integration.Api.Main.Services.Jwt
+{
+    public class JwtAuthServiceTests : MyApiIntegrationTest
+    {
+        public JwtAuthServiceTests(ApiCustomWebApplicationFactory factory) : base(factory) { }
+
+        [Fact]
+        public async Task ShouldGenerateAndValidateJwtToken()
+        {
+            var expectedUserId = 2;
+            
+            var jwtToken = JwtAuthService.BuildJwt(expectedUserId);
+
+            var isValid = JwtAuthService.IsValidJwt(jwtToken);
+            Assert.True(isValid);
+            Assert.Equal(expectedUserId, JwtAuthService.GetUserId(jwtToken));
+        }
+    }
+}
