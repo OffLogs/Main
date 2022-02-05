cad83c31d8dfacc4d99be4d5f2a64ef587ca4e4c
lampego <lampego@gmail.com>
2022-02-05 16:22:36 +0200

    Jenkins publishing scripts

--- devops/publish/api/boot.sh
+++ devops/publish/api/boot.sh
@@ -1,14 +1,4 @@
 #!/bin/bash
 
-#until dotnet ef database update; do
-#    >&2 echo "SQL Server is starting up"
-#    sleep 1
-#done
-
->&2 echo "Run Migrations"
-cd OffLogs.Migrations
-dotnet run --configuration Development
-
->&2 echo "SQL Server is up - executing command"
-cd ../OffLogs.Api
+echo "Start application.."
 dotnet run --no-launch-profile
