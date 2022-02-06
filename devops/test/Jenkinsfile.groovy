de741d93d6c5ad83d9a52531b524a3e6d1056528
lampego <lampego@gmail.com>
2022-02-06 16:23:13 +0200

    #3 Added PGSql datetime refactoring

--- devops/test/Jenkinsfile.groovy
+++ devops/test/Jenkinsfile.groovy
@@ -4,7 +4,6 @@ node('vizit-mainframe-testing-node') {
         gitLabConnection('gitlab_lampego'),
     ])
 
-
     String testScriptParameters = '--logger=trx --no-restore --no-build --results-directory=./results'
     String postresUserPassword = 'postgres'
 
