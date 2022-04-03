7f5e8685b537bdbe179ee773d94c1193babf14ef
lampego <lampego@gmail.com>
2022-04-03 11:26:04 +0300

    Minor changes

--- OffLogs.Web/Constants/SiteUrl.cs
+++ OffLogs.Web/Constants/SiteUrl.cs
@@ -4,6 +4,9 @@ namespace OffLogs.Web.Constants
     {
         public static readonly string Main = "/";
         
+        public static readonly string Registration_Step1 = "/registration/step1";
+        public static readonly string Registration_Step2 = "/registration/step2";
+        
         public static readonly string Dashboard = "/dashboard";
         public static readonly string DashboardApplications = "/dashboard/applications";
         public static readonly string DashboardLogs = "/dashboard/logs";
