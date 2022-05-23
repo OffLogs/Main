33efad8c0874887c450466e2442e9c4c5dce5503
lampego <lampego@gmail.com>
2022-05-23 08:23:42 +0300

    Radzen integration. Added logLevels dropdown

---
+++ OffLogs.Web/Shared/Ui/Form/DropDowns/LogLevelDropDown.razor.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Components;
+using OffLogs.Business.Common.Constants;
+
+namespace OffLogs.Web.Shared.Ui.Form.DropDowns;
+
+public partial class LogLevelDropDown
+{
+    [Parameter]
+    public LogLevel? Value
+    {
+        get => _value;
+        set => _value = value ?? default;
+    }
+    
+    [Parameter]
+    public EventCallback<LogLevel?> ValueChanged { get; set; }
+    
+    private List<LogLevel> _list;
+    private LogLevel _value;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        _list = Enum.GetValues(typeof(LogLevel)).Cast<LogLevel>().ToList();
+    }
+}
