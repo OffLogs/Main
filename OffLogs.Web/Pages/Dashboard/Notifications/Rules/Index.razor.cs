61d49e325c20af63ccad20178179f74e6438fc7c
lampego <lampego@gmail.com>
2022-05-24 14:02:49 +0300

    Radzen integration. Rules page

--- OffLogs.Web/Pages/Dashboard/Notifications/Rules/Index.razor.cs
+++ OffLogs.Web/Pages/Dashboard/Notifications/Rules/Index.razor.cs
@@ -1,6 +1,23 @@
-﻿namespace OffLogs.Web.Pages.Dashboard.Notifications.Rules;
+﻿using System.Threading.Tasks;
+using Fluxor;
+using Microsoft.AspNetCore.Components;
+using OffLogs.Api.Common.Dto.Entities;
+using OffLogs.Web.Store.Notification;
+using Radzen.Blazor;
+
+namespace OffLogs.Web.Pages.Dashboard.Notifications.Rules;
 
 public partial class Index
 {
+    [Inject] 
+    private IState<NotificationRuleState> _state { get; set; }
     
+    private RadzenDataGrid<NotificationRuleDto> _grid;
+
+    protected override async Task OnInitializedAsync()
+    {
+        await base.OnInitializedAsync();
+        
+        Dispatcher.Dispatch(new FetchNotificationRulesAction());
+    }
 }
