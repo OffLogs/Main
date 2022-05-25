ac84d5f0c9bcb07da5eabbd207aae3d237e4d7ba
lampego <lampego@gmail.com>
2022-05-25 17:51:44 +0300

    Radzen integration. Changed layouts

---
+++ OffLogs.Web/Pages/Dashboard/Shared/Layout.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OffLogs.Web.Pages.Dashboard.Shared;
+
+public partial class Layout: IDisposable
+{
+    protected override async Task OnInitializedAsync()
+    {
+        IsRedirectIfNotLoggedIn = true;
+        await base.OnInitializedAsync();
+    }
+}
---
+++ OffLogs.Web/Pages/Docs/Shared/Layout.razor.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace OffLogs.Web.Pages.Docs.Shared;
+
+public partial class Layout: IDisposable
+{
+    protected override async Task OnInitializedAsync()
+    {
+        IsRedirectIfNotLoggedIn = false;
+        await base.OnInitializedAsync();
+    }
+}
