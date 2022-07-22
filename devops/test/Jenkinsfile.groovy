bca194340e2afe4e04d84814b3642e4800aaac2f
lampego <lampego@gmail.com>
2022-07-22 23:32:19 +0300

    OF-95 Redis tests integration

--- devops/test/Jenkinsfile.groovy
+++ devops/test/Jenkinsfile.groovy
@@ -90,7 +90,7 @@ node('testing-node') {
             }
 
             runStage(Stage.INIT_REDIS) {
-                sh 'systemctl restart redis.service'
+                sh '/usr/bin/redis-server &'
                 sh 'until nc -z localhost 6379; do sleep 1; done'
                 echo "Redis is started"
             }
