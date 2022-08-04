186ba3e2c20eff30fbff4dec40aaa61863e48533
lampego <lampego@gmail.com>
2022-08-04 21:40:28 +0300

    #267 Jenkins integration

---
+++ devops/test-github/Jenkinsfile.groovy
@@ -0,0 +1,35 @@
+node('testing-node') {
+    properties([
+        disableConcurrentBuilds(),
+    ])
+
+    
+
+    stage("Test test") {
+        updateGithubCommitStatus("Hello message", "SUCCESS");
+    }
+}
+
+def getRepoURL() {
+  sh "git config --get remote.origin.url > .git/remote-url"
+  return readFile(".git/remote-url").trim()
+}
+
+def getCommitSha() {
+  sh "git rev-parse HEAD > .git/current-commit"
+  return readFile(".git/current-commit").trim()
+}
+
+def updateGithubCommitStatus(String message, String state) {
+  // workaround https://issues.jenkins-ci.org/browse/JENKINS-38674
+  repoUrl = getRepoURL()
+  commitSha = getCommitSha()
+
+  step([
+    $class: 'GitHubCommitStatusSetter',
+    reposSource: [$class: "ManuallyEnteredRepositorySource", url: repoUrl],
+    commitShaSource: [$class: "ManuallyEnteredShaSource", sha: commitSha],
+    errorHandlers: [[$class: 'ShallowAnyErrorHandler']],
+    statusResultSource: [ $class: "ConditionalStatusResultSource", results: [[$class: "AnyBuildResult", message: message, state: state]] ]
+  ])
+}
